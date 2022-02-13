4b60aa4d5cc5b336e9ef3ba297ae0a036670952a
uendel.couto <uendel.couto@gmail.com>
2022-02-13 14:09:17 -0300

    create category test

---
+++ tests/Unit/app/Models/CategoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\app\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
