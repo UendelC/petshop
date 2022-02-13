b1b802e2324a202dbe28c8c4800d7e76d5a9a0ee
uendel.couto <uendel.couto@gmail.com>
2022-02-13 14:07:41 -0300

    add payment factory

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Payment;
+use Closure;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,43 @@ class PaymentFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(
+            [
+                'credit_card',
+                'cash_on_delivery',
+                'bank_transfer',
+            ]
+        );
+
+        $details = match ($type) {
+            'credit_card' => json_encode(
+                [
+                    'holder_name' => $this->faker->name(),
+                    'number' => $this->faker->creditCardNumber(),
+                    'ccv' => $this->faker->creditCardDetails(),
+                    'expiration_date' => $this->faker->creditCardExpirationDate(),
+                ]
+            ),
+            'cash_on_delivery' => json_encode(
+                [
+                    'first_name' => $this->faker->firstName(),
+                    'last_name' => $this->faker->lastName(),
+                    'address' => $this->faker->address(),
+                ]
+            ),
+            'bank_transfer' => json_encode(
+                [
+                    'swift' => $this->faker->swiftBicNumber(),
+                    'iban' => $this->faker->iban(),
+                    'name' => $this->faker->name(),
+                ]
+            ),
+        };
+
         return [
-            //
+            'uuid' => $this->faker->uuid(),
+            'type' => $type,
+            'details' => $details,
         ];
     }
 }
