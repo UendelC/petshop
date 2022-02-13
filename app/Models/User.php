4269f805e58672fa7bc16e60a5822be14af52204
uendel.couto <uendel.couto@gmail.com>
2022-02-13 10:11:31 -0300

    add relationship user - order

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(File::class, 'uuid', 'avatar');
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
