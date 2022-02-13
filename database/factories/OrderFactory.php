f2256beb75d94efb3b27acdaa45cf2f55617d318
uendel.couto <uendel.couto@gmail.com>
2022-02-13 14:08:24 -0300

    add order factory definition

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\OrderStatus;
+use App\Models\Payment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,25 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'order_status_id' => OrderStatus::factory()->create()->id,
+            'payment_id' => Payment::factory()->create()->id,
+            'uuid' => $this->faker->uuid(),
+            'products' => json_encode(
+                [
+                    [
+                        'product' => $this->faker->uuid(),
+                        'quantity' => $this->faker->randomNumber(),
+                    ],
+                ]
+            ),
+            'address' => json_encode(
+                [
+                    'billing' => $this->faker->word(),
+                    'shipping' => $this->faker->word(),
+                ]
+            ),
+            'delivery_fee' => $this->faker->randomNumber(4),
+            'amount' => $this->faker->randomNumber(4),
         ];
     }
 }
